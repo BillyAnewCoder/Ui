e580064c4f5f71e7046c369faf6054416481cb10
BillyAnewCoder <billyismynewgoat@gmail.com>
2024-06-10 15:36:19 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,2 +1,14 @@
-local MyUILib = require(script.src.MyUILib)
+local MyUILib = {}
+
+local Components = {
+    "Button",
+    "Frame",
+    "Slider",
+    "Draggable"
+}
+
+for _, component in ipairs(Components) do
+    MyUILib[component] = require(script:WaitForChild("src"):WaitForChild("Components"):WaitForChild(component))
+end
+
 return MyUILib
